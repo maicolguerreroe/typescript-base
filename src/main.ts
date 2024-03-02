c6a3c566c51c68e29e68f0ea5c54533710994c6f
Maicol Guerrero <maicol.guerrero@databits.ai>
2024-03-02 14:22:55 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Proyecto base</h1>
+  <span>Revisar la consola de JavaScript</span>
+`
+
